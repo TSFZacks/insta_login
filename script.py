b3bbe9e31b04439ae6cd11c23da6e121b97ae083
TSFZacks <isaackv.s.amaral@gmail.com>
2024-08-09 17:23:51 -0300

    code is done

--- script.py
+++ script.py
@@ -1,13 +1,21 @@
-
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify
 import requests
-import json
+import os
+from dotenv import load_dotenv
+import threading
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
+import webbrowser
+
+# Configuração do Flask
+load_dotenv()
 
 app = Flask(__name__)
 
-app_id = '556346486573341'
-app_secret = '7226a077fa336c63b4ebb042d66d1dcb'
+app_id = os.getenv('APP_ID')
+app_secret = os.getenv('APP_SECRET')
 redirect_uri = 'http://localhost:8000/callback'
 
 @app.route('/')
@@ -30,19 +38,39 @@ def callback():
     }
 
     response = requests.get(token_url, params=token_params)
+    
+    if response.status_code != 200:
+        return f'Erro ao obter token de acesso: {response.text}', response.status_code
 
-    time.sleep(3)
-
-    access_token_info = json.loads(str(response.text))
+    access_token_info = response.json()
     access_token = access_token_info.get('access_token')
 
     if not access_token:
         return 'Erro ao obter token de acesso.', 400
     
-    print(access_token)
+    # Armazenar o token de acesso em um arquivo
+    with open('insta_login/token.txt', 'w') as token_file:
+        token_file.write(access_token)
+    
+    # Retornar uma resposta informando que o token foi salvo
+    return jsonify(message='Token de acesso salvo com sucesso em token.txt')
 
-if __name__ == '__main__':
+def run_flask():
     app.run(port=8000)
 
+def open_browser():
 
+    time.sleep(4)
+    webbrowser.open('http://127.0.0.1:8000')
 
+if __name__ == '__main__':
+    # Criar e iniciar as threads
+    flask_thread = threading.Thread(target=run_flask)
+    selenium_thread = threading.Thread(target=open_browser)
+    
+    flask_thread.start()
+    selenium_thread.start()
+    
+    # Esperar que ambas as threads terminem
+    flask_thread.join()
+    selenium_thread.join()
